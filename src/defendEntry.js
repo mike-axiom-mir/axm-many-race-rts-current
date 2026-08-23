f26a4e48e3a6a0e24e6417cc8da14b8ebe13a7c2
mike-axiom-mir <miketobi90@gmail.com>
2026-08-23 12:35:50 +0200

    Phase 19: keep detailed formation visuals in Workshop Defense

--- src/defendEntry.js
+++ src/defendEntry.js
@@ -1,6 +1,7 @@
 import "./defenseRosterRegistryPatch.js";
 import "./combatDepthPatch.js";
 import "./rosterAnimationPatch.js";
+import "./unitBuildingVisualPatch.js";
 import "./defenseRosterVisualPatch.js";
 import "./defendWorkshop.js";
 import "./defendLifecyclePatch.js";
