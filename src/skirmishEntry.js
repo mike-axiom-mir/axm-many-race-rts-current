942de0e100a25f073f4fac278c0329dcf9e7bf9a
mike-axiom-mir <miketobi90@gmail.com>
2026-08-23 09:53:06 +0200

    Add seat command authority gate to skirmish entry

--- src/skirmishEntry.js
+++ src/skirmishEntry.js
@@ -1,5 +1,6 @@
 import "./battlePatch.js";
 import "./multiSeatPatch.js";
+import "./seatCommandAuthorityPatch.js";
 import "./multiSeatVictoryPatch.js";
 import "./multiSeatMapPatch.js";
 import "./multiSeatHud.js";
