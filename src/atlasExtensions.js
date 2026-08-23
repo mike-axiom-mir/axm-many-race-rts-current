67eb1b2fff17c07b52ee0f24d6130583b806ece9
mike-axiom-mir <miketobi90@gmail.com>
2026-08-23 10:08:39 +0200

    Extend Atlas with upgrade and Workshop content categories

---
+++ src/atlasExtensions.js
@@ -0,0 +1,22 @@
+import { ATLAS_TYPES, registerAtlasEntries } from "./atlasRegistry.js";
+import { DEFEND_ATLAS_ENTRIES, DEFEND_UPGRADES } from "./defendConfig.js";
+
+if (!ATLAS_TYPES.some(type => type.id === "upgrade")) {
+  const modeIndex = ATLAS_TYPES.findIndex(type => type.id === "mode");
+  ATLAS_TYPES.splice(modeIndex < 0 ? ATLAS_TYPES.length : modeIndex, 0, { id: "upgrade", label: "Upgrades & research", icon: "⬆" });
+}
+
+registerAtlasEntries([
+  ...DEFEND_ATLAS_ENTRIES.map(entry => ({ ...entry, source: "builtin-defend-workshop" })),
+  ...DEFEND_UPGRADES.map(upgrade => ({
+    id: `upgrade:defend-workshop:${upgrade.id}`,
+    type: "upgrade",
+    name: upgrade.name,
+    icon: upgrade.icon,
+    subtitle: "Defend the Workshop run upgrade",
+    summary: upgrade.description,
+    tags: ["defend-workshop", "wave-upgrade", upgrade.id],
+    stats: Object.fromEntries(Object.entries(upgrade.effect || {}).map(([key, value]) => [key, value])),
+    source: "builtin-defend-workshop"
+  }))
+]);
