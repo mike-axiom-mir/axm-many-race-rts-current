a47f598dd3051ab4e9cbdc3c01d6ea3af767c3bc
mike-axiom-mir <miketobi90@gmail.com>
2026-08-23 11:38:18 +0200

    Resolve evenly spaced live starts for selected maps

--- src/selectedMapPatch.js
+++ src/selectedMapPatch.js
@@ -1,4 +1,5 @@
 import { DEFAULT_MAP, getMapById } from "./maps.js";
+import { currentLiveSeatCount, runtimeMapStarts } from "./runtimeMapStarts.js";
 
 function selectedFlatMap() {
   try {
@@ -11,11 +12,19 @@ function selectedFlatMap() {
   }
 }
 
-const selected = selectedFlatMap();
-if (selected !== DEFAULT_MAP) {
+const source = selectedFlatMap();
+const liveStarts = runtimeMapStarts(source, currentLiveSeatCount());
+
+if (source !== DEFAULT_MAP) {
+  const copy = JSON.parse(JSON.stringify(source));
   for (const key of Object.keys(DEFAULT_MAP)) delete DEFAULT_MAP[key];
-  Object.assign(DEFAULT_MAP, JSON.parse(JSON.stringify(selected)));
+  Object.assign(DEFAULT_MAP, copy);
 }
 
+if (liveStarts[0]) DEFAULT_MAP.playerStart = [...liveStarts[0]];
+if (liveStarts[1]) DEFAULT_MAP.enemyStart = [...liveStarts[1]];
+else if (liveStarts[0]) DEFAULT_MAP.enemyStart = [-liveStarts[0][0], liveStarts[0][1], -liveStarts[0][2]];
+DEFAULT_MAP.runtimeStarts = liveStarts.map(point => [...point]);
+
 window.__AXM_ACTIVE_MAP__ = DEFAULT_MAP;
-window.dispatchEvent(new CustomEvent("axm-active-map-ready", { detail: { mapId: DEFAULT_MAP.id } }));
+window.dispatchEvent(new CustomEvent("axm-active-map-ready", { detail: { mapId: DEFAULT_MAP.id, starts: DEFAULT_MAP.runtimeStarts } }));
